6dd06dafa61d2076754db2a4814e31a2f37c41dd
Soon Siang <jymchng@hotmail.com>
2023-02-27 23:29:14 +0800

    cls.getattr(intern!(cls.py(), validate)) doesnt work

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ use crate::core::NRIC;
 use crate::python::{PyNRIC, PyNRICContainer};
 use pyo3::prelude::*;
 
-/// A Python module implemented in Rust.
+// A Python module implemented in Rust.
 #[pymodule]
 fn sgnric_do_not_use(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyNRIC>()?;
--- src/python.rs
+++ src/python.rs
@@ -3,15 +3,18 @@ use crate::core::NRIC;
 use crate::digits::ICDigits;
 use crate::prefix::ICPrefixEnum;
 use crate::suffix::ICSuffixEnum;
+use pyo3::exceptions::{PyValueError, PyAttributeError};
+use pyo3::intern;
 use pyo3::{
     class::iter::IterNextOutput,
     exceptions::{PyException, PyStopIteration},
     marker::Python,
     prelude::*,
-    types::{PyDict, PyString, PyType, PyAny},
+    types::{PyDict, PyString, PyType, PyAny, PyIterator, PyFunction, PyCFunction},
 };
 use std::fmt;
 use std::vec::IntoIter;
+
 
 #[pyclass(name = "NRIC")]
 #[derive(Debug, Clone)]
@@ -21,6 +24,7 @@ pub struct PyNRIC {
 
 #[pyclass(name = "PyNRICContainer")]
 #[derive(Debug)]
+#[pyo3(text_signature = "(cls, value, values, config, field)")]
 pub struct PyNRICContainer {
     #[pyo3(get, set)]
     boolean: bool,
@@ -39,54 +43,78 @@ impl fmt::Display for PyNRIC {
 #[pymethods]
 impl PyNRIC {
     #[new]
-    fn new(string: String) -> PyResult<PyNRIC> {
-        Ok(PyNRIC {
-            rust_nric: NRIC::new(string).unwrap(),
-        })
+    pub fn new(string: String) -> PyResult<PyNRIC> {
+        let new_nric = NRIC::new(string);
+        match new_nric {
+            Ok(new_nric) => Ok(PyNRIC {
+                rust_nric: new_nric
+            }),
+            Err(err) => Err(PyValueError::new_err(err)),
+        }
     }
 
     #[getter]
-    fn get_prefix(&self) -> PyResult<String> {
+    pub fn get_prefix(&self) -> PyResult<String> {
         Ok(self.rust_nric.prefix.to_string())
     }
 
     #[getter]
-    fn get_suffix(&self) -> PyResult<String> {
+    pub fn get_suffix(&self) -> PyResult<String> {
         Ok(self.rust_nric.suffix.to_string())
     }
 
     #[getter]
-    fn get_digits(&self) -> PyResult<String> {
+    pub fn get_digits(&self) -> PyResult<String> {
         Ok(self.rust_nric.digits.to_string())
     }
 
-    fn __str__(&self) -> PyResult<String> {
+    pub fn __str__(&self) -> PyResult<String> {
         Ok(self.to_string())
     }
 
-    // fn __repr__(&self) -> PyResult<String> {
-    //     Ok(format!("<NRIC::{}>", self.to_string()))
-    // }
+    pub fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("<NRIC::{}>", self.to_string()))
+    }
 
     #[classmethod]
-    fn __get_validators__(_cls: &PyType) -> PyResult<PyNRICContainer> {
-        Ok(PyNRICContainer {boolean: true})
+    pub fn __get_validators__(cls: &PyType) -> PyResult<&PyIterator> {
+        let func = cls.getattr(intern!(cls.py(), "validate"));
+        match func {
+            Ok(func) => {
+                if let Ok(funciter) = func.iter() {
+                    return Ok(funciter)
+                } else {
+                    return Err(PyAttributeError::new_err("`validate` method is not found!"))
+                }
+            },
+            Err(err) => Err(err),
+        }
     }
+
+    #[classmethod]
+    pub fn validate(cls: &PyType, value: &PyString) -> PyResult<PyNRIC> {
+        let v: String = value.extract()?;
+        PyNRIC::new(v)
+    }
+
+    // fn __call__(&self) -> PyResult<&str> {
+    //     Ok("Hello, World!")
+    // }
 }
 
 #[pymethods]
 impl PyNRICContainer {
 
-    // #[new]
-    // fn new() -> PyResult<PyNRICContainer> {
-    //     Ok(PyNRICContainer { boolean: true })
-    // }
+    #[new]
+    pub fn new() -> PyResult<PyNRICContainer> {
+        Ok(PyNRICContainer { boolean: true })
+    }
 
-    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+    pub fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
         slf
     }
 
-    fn __next__(mut slf: PyRefMut<'_, Self>) -> IterNextOutput<PyRefMut<'_, Self>, &'static str> {
+    pub fn __next__(mut slf: PyRefMut<'_, Self>) -> IterNextOutput<PyRefMut<'_, Self>, &'static str> {
         if slf.boolean {
             slf.boolean = false;
             IterNextOutput::Yield(slf)
@@ -95,16 +123,20 @@ impl PyNRICContainer {
         }
     }
 
-    #[classmethod]
-    fn __call__(cls: &PyType, v: &PyString) -> PyResult<PyNRIC> {
-        let v: String = v.extract()?;
+    #[staticmethod]
+    pub fn __call__(value: &PyString, values: &PyDict, config: &PyAny, field: &PyAny) -> PyResult<PyNRIC> {
+        let v: String = value.extract()?;
         PyNRIC::new(v)
     }
-
-    // #[pyo3(signature = "cls", "v", "values")]
-    // fn __call__(&mut self, py: Python<'_>, value: &PyString, values: Option<&PyDict>) -> PyResult<PyNRIC> {
-    //     let v: String = value.extract()?;
-    //     PyNRIC::new(v)
-    // }
     
 }
+
+
+#[pyfunction]
+#[pyo3(text_signature = "(cls, value, values, config, field)")]
+pub fn validate(cls: &PyType, value: &PyString) -> PyResult<PyNRIC> {
+    let v: String = value.extract()?;
+    PyNRIC::new(v)
+}
+
+
