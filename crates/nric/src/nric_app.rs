a8f010a26caffa78bde9c61a60ab77d7ec9f7a8b
Soon Siang <jymchng@hotmail.com>
2023-03-06 00:07:27 +0800

    can compile

---
+++ crates/nric/src/nric_app.rs
@@ -0,0 +1,34 @@
+mod builder;
+mod digits;
+mod prefix;
+mod suffix;
+
+#[cfg(feature = "python")]
+pub mod python;
+
+pub mod nric;
+
+use crate::nric::NRIC;
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("nric")
+        .version("1.0")
+        .author("DO NOT USE!")
+        .about("CLI to validate NRIC from a text, DO NOT USE!")
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Your Input String")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    let input = matches.value_of("INPUT").unwrap();
+    let nric = NRIC::new(input);
+
+    match nric {
+        Ok(_) => println!("Valid"),
+        Err(_) => println!("Invalid"),
+    }
+}
